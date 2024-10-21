52a3c912f4b15d0b7227a506d5c5046e2fd722ba
José Pérez <105478390+JoseDPerez11@users.noreply.github.com>
2024-10-21 17:38:14 -0500

    creating project section and modal

---
+++ src/components/FullProjectModal.tsx
@@ -0,0 +1,44 @@
+import { Badge, Button, Group, Image, Indicator, Modal, ScrollArea, Text } from "@mantine/core"
+
+const FullProjectModal=(props: any)=> {
+    return <Modal.Root scrollAreaComponent={ScrollArea.Autosize} size="70%" className="font-mono" opened={props.opened} onClose={props.close} >
+        <Modal.Overlay />
+        <Modal.Content className="!rounded-3xl">
+
+            <Modal.Header className="!bg-bgColor !border-primaryColor !border-2 !border-b-0 !rounded-tl-3xl !rounded-tr-3xl" >
+                <Modal.Title className="!text-4xl text-white flex gap-3 items-center font-bold" > {props.title}{props.live === true && <Badge className="flex items-center gap-1" size="lg" variant="outline" color="red" rightSection={<Indicator color="red" position="middle-end" size={10} processing></Indicator>} >Live</Badge>} </Modal.Title>
+                <Modal.CloseButton size="md" iconSize="30px" className="!bg-bgColor !text-red-500" />
+            </Modal.Header>
+
+            <Modal.Body className="!bg-bgColor !pt-2 !border-primaryColor !border-2 !border-t-0 !rounded-bl-3xl !rounded-br-3xl" >
+                <Image
+                    className="!rounded-xl !shadow-[0_0_5px_0_#64FFDA]"
+                    src={props.image}
+                    alt={props.image}
+                 />
+                
+                <Group mt="md" mb="md" >
+                    {props.technologies.map((tech: string, index: number, ) => index < 3 && <Badge key={index} size="xl" variant="!light" color="64FFDA"> {tech} </Badge>)}
+                </Group>
+                <Text className="!text-justify" size="lg" c="dimmed" >
+                    {props.desc}
+                </Text>
+                <Group justify="space-between" mt="md" >
+                    <a href={props.github} target="_blank" className="!w-[48%]">
+                        <Button variant="outline" size="lg"color="#64FFDA" fullWidth mt="md" radius="md" >
+                            view code
+                        </Button>
+                    </a>
+                    <a href={props.link} target="_blank" className="!w-[48%]">
+                        <Button size="lg" color="#64FFDA" className="!text-bgColor" fullWidth mt="md" radius="md" >
+                            view live app
+                        </Button>
+                    </a>
+                </Group>
+            </Modal.Body>
+        </Modal.Content>
+    </Modal.Root>
+
+}
+
+export default FullProjectModal;
---
+++ src/components/ProjectCard.tsx
@@ -0,0 +1,37 @@
+import { Badge, Button, Card, Group, Image, Indicator, Text } from "@mantine/core"
+import { useDisclosure } from "@mantine/hooks";
+import FullProjectModal from "./FullProjectModal";
+
+const ProjectCard=(props: any) => {
+    const [opened, { open, close }] = useDisclosure(false);
+    return <><Card onClick={open} className="!bg-bgColor cursor-pointer translation-transform duration-300 ease-in-out hover:!scale-[1.03] mb-5 hover:!shadow-[0_0_10px_1px_#64FFDA] !border-primaryColor border-2" w="360px" shadow="lg" padding="sm" radius="lg" withBorder >
+            <Card.Section className="p-3" >
+                <Image
+                    className="!rounded-xl !shadow-[0_0_5px_0_#64FFDA]"
+                  //src={props.image}
+                  //alt={props.image}
+                 />
+            </Card.Section>
+
+            <Group justify="space-between" mt="xs" mb="xs" >
+                <div className="!text-2xl gap-2 !font-bold !text-white flex items-center sm-mx:!text-xl" >{props.title}{props.live === true && <Badge className="!px-1" variant="outline" color="red" rightSection={<Indicator className="!mr-0.5 !z-0" color="red" position="middle-end" size={7} processing></Indicator>} >Live</Badge>}</div>
+            </Group>
+
+            <Group mt="xs" mb="sm" >
+                {props.technologies.map((tech: string, index: number) => index < 3 && <Badge key={index} size="lg" variant="light" color="#64FFDA">{tech}</Badge>)}
+            </Group>
+
+            <Text className="!text-justify" lineClamp={5} size="sm" c="dimmed" >
+                {props.desc}
+            </Text>
+
+            <Button onClick={open} className="!bg-primaryColor !text-bgColor" fullWidth mt="md" radius="md" >
+                show more
+            </Button>
+        </Card>
+
+        <FullProjectModal opened={opened} close={close} title={props.title} desc={props.desc} image={props.image} live={props.live} link={props.link} github={props.github} technologies={props.technologies} />
+
+        </>
+}
+export default ProjectCard;
